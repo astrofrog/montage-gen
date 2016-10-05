ad3d46d3c47e7f1241cd68571251759b84624c71
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-10-05 09:19:27 +0100

    Added initial version of Travis configuration

--- cython/setup.py
+++ cython/setup.py
@@ -4,19 +4,25 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-LIB = os.path.join('..', '..', 'Montage', 'lib')
-MONTAGELIB = os.path.join('..', '..', 'Montage', 'MontageLib')
+if os.path.exists(os.path.join('..', 'Montage')):
+    LIB = os.path.join('..', 'Montage', 'lib')
+    MONTAGELIB = os.path.join('..', 'Montage', 'MontageLib')
+elif os.path.exists(os.path.join('..', '..', 'Montage')):
+    LIB = os.path.join('..', '..', 'Montage', 'lib')
+    MONTAGELIB = os.path.join('..', '..', 'Montage', 'MontageLib')
+else:
+    raise ValueError("Could not find the Montage directory")
 
 extensions = [
     Extension("montage_direct._wrappers", ["montage_direct/_wrappers.pyx"],
         include_dirs = [os.path.join(LIB, 'include'), MONTAGELIB],
         libraries = ['wcs', 'coord','mtbl','cfitsio','m', 'boundaries', 'freetype', 'twoplane', 'pixbounds', 'www', 'json', 'lodepng', 'cmd'],
-        library_dirs = [LIB],
+        library_dirs = [LIB, os.path.join(LIB, 'freetype', 'lib')],
         extra_objects = [os.path.join(MONTAGELIB, 'libmontage.a'), os.path.join(LIB, 'libjpeg.a')]),
     Extension("montage_direct.main", ["montage_direct/main.pyx"],
         include_dirs = [os.path.join(LIB, 'include'), MONTAGELIB],
         libraries = ['wcs', 'coord','mtbl','cfitsio','m', 'boundaries', 'freetype', 'twoplane', 'pixbounds', 'www'],
-        library_dirs = [LIB],
+        library_dirs = [LIB, os.path.join(LIB, 'freetype', 'lib')],
         extra_objects = [os.path.join(MONTAGELIB, 'libmontage.a')])
 ]
 
